adee69a3cf9c825422466a8e1f2d60ef3ecaf788
He0x539 <43481044+He0x539@users.noreply.github.com>
2019-05-07 18:00:30 +0200

    Update GeoLoc.py

--- GeoLoc.py
+++ GeoLoc.py
@@ -4,7 +4,7 @@ import json
 class LanguageFetch:
 
     def __init__(self):
-        access_key = '2949d1223fe896b54cbffcc0aa0ec46d'
+        access_key = ''
         ip = urlopen('http://ip.42.pl/anything').read().decode('UTF-8') # test UK 185.86.151.11
         #ip = '185.86.151.11'
         webcall_str = "http://api.ipstack.com/" + ip + "?access_key=" + access_key
